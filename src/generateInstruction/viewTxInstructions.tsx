17ac8299290610a2a7cb65734761436cda30b5bd
thiennv <thiennguyen@descartes.network>
2022-05-31 17:24:39 +0700

    expand

--- src/generateInstruction/viewTxInstructions.tsx
+++ src/generateInstruction/viewTxInstructions.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 
 import { useParser } from 'providers/parser.provider'
+import Expand from 'components/expand'
 
 const ViewTxInstructions = () => {
   const { txInstructions } = useParser()
@@ -10,12 +11,16 @@ const ViewTxInstructions = () => {
   const keyTxInstructs = Object.keys(txInstructions || {})
 
   return (
-    <div className="grid gird-cols-1 gap-4 p-4 bg-[#EBEBEB]">
+    <div className="grid gird-cols-1 gap-4 p-4 bg-[#1A1311] text-white">
       {keyTxInstructs.map((key, idx) => {
         const data = txInstructions[key]
         return (
           <div className="flex flex-col gap-4" key={idx}>
-            <pre className="flex flex-col">{JSON.stringify(data, null, 2)}</pre>
+            <Expand header="Header">
+              <pre className="flex flex-col">
+                {JSON.stringify(data, null, 2)}
+              </pre>
+            </Expand>
           </div>
         )
       })}

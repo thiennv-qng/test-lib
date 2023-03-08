cd7a78011e311a3b60225fa25807b74d44778857
thiennv <thiennguyen@descartes.network>
2023-03-08 16:56:09 +0700

    resolve #7 resolves this task

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -5,6 +5,7 @@ import ViewUploaded from 'upload/viewUploaded'
 
 const IdlParserWrapper = () => {
   // add test task 04.1
+  // add new
   return (
     <div className="grid grid:cols-1 gap-6">
       <div className="flex w-full flex-col md:flex-row gap-10 md:gap-6">
